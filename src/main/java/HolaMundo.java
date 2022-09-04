c9a630a28104c24482dca83f2210c419063d0f9c
Berna <bernardo.sandoval13@gmail.com>
2022-09-03 23:02:40 -0400

    Bernardo Sandoval hola mundo2

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo Leandro Pino");
+
         System.out.println("Hola mundo Bernardo Sandoval");
     }
 }
